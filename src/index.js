c58a8aa17257abcc0d57a8da5559e85fbf5b68b7
Daria Savicheva <savicheva.darya@hexly.ru>
2023-03-28 14:15:05 -0700

    add function gendiff src/index

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ const genDiff = (path1, path2) => {
   const data2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path2), 'utf-8'));
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(keys);
+  let result = '';
   for (const key of sortedKeys) {
     if (data1.hasOwnProperty(key) && !data2.hasOwnProperty(key)) {
-      let result = `${result}\n- ${[key]}: ${data1[key]}`;
+      result = `- ${[key]}: ${data1[key]}`;
+      console.log(result);
+    } else if ((data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) && data1[key] === data2[key]) {
+      result = `  ${[key]}: ${data1[key]}`;
+      console.log(result);
+    } else if ((data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) && data1[key] !== data2[key]){
+      result = `- ${[key]}: ${data1[key]}`;
+      console.log(result);
+      result = `+ ${[key]}: ${data2[key]}`;
+      console.log(result);
+    } else {
+      result = `+ ${[key]}: ${data2[key]}`;
     }
   }
   return result;
