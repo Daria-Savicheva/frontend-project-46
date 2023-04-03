10aa6c1a08cc8b537516656ece6121647f977586
Daria Savicheva <savicheva.darya@hexly.ru>
2023-04-03 04:00:06 -0700

    step 3

--- src/index.js
+++ src/index.js
@@ -2,30 +2,41 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
+const bildPath = (way) => path.resolve(process.cwd(), way);
+
 const genDiff = (path1, path2) => {
-  const data1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path1), 'utf-8'));
+  const absolutePath1 = bildPath(path1);
+  const absolutePath2 = bildPath(path2);
+
+  const getData1 = fs.readFileSync(absolutePath1, 'utf-8');
+  const getData2 = fs.readFileSync(absolutePath2, 'utf-8');
+
+  const data1 = JSON.parse(getData1);
+  const data2 = JSON.parse(getData2);
   // const data1 = getData(bildPath(path1))
-  const data2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), path2), 'utf-8'));
+  
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(keys);
   let result = '';
+  console.log('{');
   for (const key of sortedKeys) {
-    if (data1.hasOwnProperty(key) && !data2.hasOwnProperty(key)) {
+    if (!data2.hasOwnProperty(key)) {
       result = `- ${[key]}: ${data1[key]}`;
       console.log(result);
-    } else if ((data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) && data1[key] === data2[key]) {
-      result = `  ${[key]}: ${data1[key]}`;
+    } else if (!data1.hasOwnProperty(key)) {
+      result = `+ ${[key]}: ${data2[key]}`;
       console.log(result);
-    } else if ((data1.hasOwnProperty(key) && data2.hasOwnProperty(key)) && data1[key] !== data2[key]){
+    } else if (data1[key] !== data2[key]){
       result = `- ${[key]}: ${data1[key]}`;
       console.log(result);
       result = `+ ${[key]}: ${data2[key]}`;
       console.log(result);
     } else {
-      result = `+ ${[key]}: ${data2[key]}`;
+      result = `  ${[key]}: ${data2[key]}`;
+      console.log(result);
     }
   }
-  return result;
+  return '}';
 };
 
 export default genDiff;
