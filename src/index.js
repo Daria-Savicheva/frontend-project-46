9e2031200b0c5b2382e6cd663c0ed6cb8c540cfe
Daria Savicheva <savicheva.darya@hexly.ru>
2023-04-10 16:15:40 +0300

    update all

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
+import compareData from './compareDate.js';
+import getFormat from './formates/index.js';
 
 const bildPath = (way) => path.resolve(process.cwd(), way);
 
-const genDiff = (path1, path2) => {
+const genDiff = (path1, path2, formatName = 'stylish') => {
   const absolutePath1 = bildPath(path1);
   const absolutePath2 = bildPath(path2);
 
@@ -16,26 +18,28 @@ const genDiff = (path1, path2) => {
 
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(keys);
-  let result = '';
-  console.log('{');
-  for (const key of sortedKeys) {
-    if (!Object.hasOwn(data2, key)) {
-      result = `- ${[key]}: ${data1[key]}`;
-      console.log(result);
-    } else if (!Object.hasOwn(data1, key)) {
-      result = `+ ${[key]}: ${data2[key]}`;
-      console.log(result);
-    } else if (data1[key] !== data2[key]) {
-      result = `- ${[key]}: ${data1[key]}`;
-      console.log(result);
-      result = `+ ${[key]}: ${data2[key]}`;
-      console.log(result);
-    } else {
-      result = `  ${[key]}: ${data2[key]}`;
-      console.log(result);
-    }
-  }
-  return '}';
+//  let result = '';
+//  console.log('{');
+//  for (const key of sortedKeys) {
+//    if (!Object.hasOwn(data2, key)) {
+//      result = `- ${[key]}: ${data1[key]}`;
+//      console.log(result);
+//    } else if (!Object.hasOwn(data1, key)) {
+//      result = `+ ${[key]}: ${data2[key]}`;
+//      console.log(result);
+//    } else if (data1[key] !== data2[key]) {
+//      result = `- ${[key]}: ${data1[key]}`;
+//      console.log(result);
+//      result = `+ ${[key]}: ${data2[key]}`;
+//  console.log(result);
+// } else {
+// result = `  ${[key]}: ${data2[key]}`;
+//  console.log(result);
+  //  }
+  // }
+  // return '}';
+  const differences = getFormat(compareData(data1, data2), formatName);
+  return differences;
 };
 
 export default genDiff;
