19c33dd770aff70457dd742bfcdf0b75e028d947
Daria Savicheva <savicheva.darya@hexly.ru>
2023-04-11 19:05:42 +0300

    update tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,8 +1,16 @@
-import _ from 'lodash';
 import fs from 'fs';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'node:url';
 import genDiff from '../src/index.js';
 
-test('is work 1', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual('}');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('diff two files', () => {
+  const path1 = getFixturePath('file1.json');
+  const path2 = getFixturePath('file2.json');
+  const value = '{\n+  follow:false\n   host:hexlet.io\n+  proxy:123.234.53.22\n-  timeout:50\n+  timeout:20\n-  verbose:true\n}';
+  expect(genDiff(path1, path2)).toEqual(value);
 });
