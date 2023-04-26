503d1a49dc3d91f5bc55b2d3657389c32f9145c7
Daria Savicheva <savicheva.darya@hexly.ru>
2023-04-26 15:25:45 +0300

    tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,24 +1,31 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'node:url';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
-// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getReadFile = (file) => {
+  const filePath = getFixturePath(file);
+  const content = fs.readFileSync(filePath, 'utf-8');
+  return content;
+};
 
-test('diff JSON files', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
-  const value = '{\n+  follow:false\n   host:hexlet.io\n+  proxy:123.234.53.22\n-  timeout:50\n+  timeout:20\n-  verbose:true\n}';
-  expect(genDiff(path1, path2)).toEqual(value);
+const firstJSON = getFixturePath('file1.json');
+const secondJSON = getFixturePath('file2.json');
+const firstYML = getFixturePath('file1.yml');
+const secondYML = getFixturePath('file2.yml');
+
+const expectedStylish = getReadFile('resultStylish.txt').trim();
+// const expectedPlain = getReadFile('resultPlain.txt').trim();
+// const expectedJSON = getReadFile('resultJSON.txt').trim();
+
+test('#1 difference stylish format test between JSON files', () => {
+  expect(genDiff(firstJSON, secondJSON, 'stylish')).toEqual(expectedStylish);
 });
 
-test('diff YAML files', () => {
-  const path1 = getFixturePath('file1.yml');
-  const path2 = getFixturePath('file2.yml');
-  const value = '{\n+  follow:false\n   host:hexlet.io\n+  proxy:123.234.53.22\n-  timeout:50\n+  timeout:20\n-  verbose:true\n}';
-  expect(genDiff(path1, path2)).toEqual(value);
+test('#2 difference stylish format test between YML files', () => {
+  expect(genDiff(firstYML, secondYML, 'stylish')).toEqual(expectedStylish);
 });
