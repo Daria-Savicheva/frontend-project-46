e0f12d6be72e8778bfccf2e66b091f7fb9abce0a
Daria Savicheva <savicheva.darya@hexly.ru>
2023-05-01 17:38:45 +0300

    update tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -21,12 +21,28 @@ const secondYML = getFixturePath('file2.yml');
 
 const expectedStylish = getReadFile('resultStylish.txt').trim();
 // const expectedPlain = getReadFile('resultPlain.txt').trim();
-// const expectedJSON = getReadFile('resultJSON.txt').trim();
+const expectedJSON = getReadFile('resultJSON.txt').trim();
 
-test('#1 difference stylish format test between JSON files', () => {
+test('#1 difference between two JSON files, format stylish', () => {
   expect(genDiff(firstJSON, secondJSON, 'stylish')).toEqual(expectedStylish);
 });
 
-test('#2 difference stylish format test between YML files', () => {
+test('#2 difference between two YML files, format stylish', () => {
   expect(genDiff(firstYML, secondYML, 'stylish')).toEqual(expectedStylish);
+});
+
+test('#3 difference between two JSON files, format json', () => {
+  expect(genDiff(firstJSON, secondJSON, 'json')).toEqual(expectedJSON);
+});
+
+test('#4 difference between YML files, format json', () => {
+  expect(genDiff(firstYML, secondYML, 'json')).toEqual(expectedJSON);
+});
+
+test('#5 difference between two JSON files, format = default', () => {
+  expect(genDiff(firstJSON, secondJSON)).toEqual(expectedStylish);
+});
+
+test('#6 difference between two YML files, format = default', () => {
+  expect(genDiff(firstYML, secondYML)).toEqual(expectedStylish);
 });
